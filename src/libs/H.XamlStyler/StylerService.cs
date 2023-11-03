5993afe45e38be97defb22e49c2e61641f7fc6d4
HavenDV <havendv@gmail.com>
2023-11-04 02:31:28 +0400

    feat: Updated to main.

--- src/libs/H.XamlStyler/StylerService.cs
+++ src/libs/H.XamlStyler/StylerService.cs
@@ -1,8 +1,10 @@
 ﻿// (c) Xavalon. All rights reserved.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
@@ -17,44 +19,26 @@ using Xavalon.XamlStyler.Services;
 
 namespace Xavalon.XamlStyler
 {
+
     public class StylerService
     {
+        private readonly string[] ignoredNamespacesInOrdering = new string[]
+        {
+            "http://schemas.microsoft.com/expression/blend/2008",
+            "http://xamarin.com/schemas/2014/forms/design",
+        };
         private readonly DocumentManipulationService documentManipulationService;
-        private readonly Dictionary<XmlNodeType, IDocumentProcessor> documentProcessors;
+        private readonly IStylerOptions options;
+        private readonly XamlLanguageOptions xamlLanguageOptions;
         private readonly XmlEscapingService xmlEscapingService;
+        private Dictionary<XmlNodeType, IDocumentProcessor> documentProcessors;
 
-        public StylerService(IStylerOptions options)
+        public StylerService(IStylerOptions options, XamlLanguageOptions xamlLanguageOptions)
         {
             this.xmlEscapingService = new XmlEscapingService();
             this.documentManipulationService = new DocumentManipulationService(options);
-
-            var indentService = new IndentService(options);
-            var markupExtensionFormatter = new MarkupExtensionFormatter(options.NoNewLineMarkupExtensions.ToList());
-            var attributeInfoFactory = new AttributeInfoFactory(new MarkupExtensionParser(), new AttributeOrderRules(options));
-            var attributeInfoFormatter = new AttributeInfoFormatter(markupExtensionFormatter, indentService);
-
-            this.documentProcessors = new Dictionary<XmlNodeType, IDocumentProcessor>
-            {
-                // { XmlNodeType.None, null },
-                { XmlNodeType.Element, new ElementDocumentProcessor(options, attributeInfoFactory, attributeInfoFormatter, indentService, xmlEscapingService) },
-                // { XmlNodeType.Attribute, null },
-                { XmlNodeType.Text, new TextDocumentProcessor(indentService) },
-                { XmlNodeType.CDATA, new CDATADocumentProcessor(indentService) },
-                // { XmlNodeType.EntityReference, null },
-                // { XmlNodeType.Entity, null },
-                { XmlNodeType.ProcessingInstruction, new ProcessInstructionDocumentProcessor(indentService) },
-                { XmlNodeType.Comment, new CommentDocumentProcessor(options, indentService) },
-                // { XmlNodeType.Document, null },
-                // { XmlNodeType.DocumentType, null },
-                // { XmlNodeType.DocumentFragment, null },
-                // { XmlNodeType.Notation, null },
-                { XmlNodeType.Whitespace, new WhitespaceDocumentProcessor() },
-                { XmlNodeType.SignificantWhitespace, new SignificantWhitespaceDocumentProcessor() },
-                { XmlNodeType.EndElement, new EndElementDocumentProcessor(options,indentService) },
-                // { XmlNodeType.EndEntity, null },
-                // ignoring xml declarations for Xamarin support
-                { XmlNodeType.XmlDeclaration, new XmlDeclarationDocumentProcessor() }
-            };
+            this.options = options;
+            this.xamlLanguageOptions = xamlLanguageOptions;
         }
 
         /// <summary>
@@ -77,6 +61,13 @@ namespace Xavalon.XamlStyler
                 // Manipulate the document tree.
                 var manipulatedDocument = this.documentManipulationService.ManipulateDocument(xDocument);
 
+                // Find ignored namespaces in document.
+                var ignoredNamespacesPrefixes = this.FindIgnoredNamespaces(manipulatedDocument);
+
+                // Once we have ignored namespaces from first element,
+                // we can apply styler configuration.
+                this.ApplyOptions(ignoredNamespacesPrefixes, options.IgnoreDesignTimeReferencePrefix);
+
                 // Format it to a string.
                 var format = this.Format(manipulatedDocument);
 
@@ -85,6 +76,81 @@ namespace Xavalon.XamlStyler
             }
 
             return xamlOutput;
+        }
+
+        private void ApplyOptions(IList<string> ignoredNamespacesPrefixes, bool ignoreDesignTimeReferencePrefix)
+        {
+            var indentService = new IndentService(options);
+            var markupExtensionFormatter = new MarkupExtensionFormatter(options.NoNewLineMarkupExtensions.ToList());
+            var attributeInfoFactory = new AttributeInfoFactory(new MarkupExtensionParser(), new AttributeOrderRules(options), ignoredNamespacesPrefixes, ignoreDesignTimeReferencePrefix);
+            var attributeInfoFormatter = new AttributeInfoFormatter(markupExtensionFormatter, indentService);
+
+            this.documentProcessors = new Dictionary<XmlNodeType, IDocumentProcessor>
+            {
+                // { XmlNodeType.None, null },
+                { XmlNodeType.Element, new ElementDocumentProcessor(options, xamlLanguageOptions, attributeInfoFactory, attributeInfoFormatter, indentService, xmlEscapingService) },
+                // { XmlNodeType.Attribute, null },
+                { XmlNodeType.Text, new TextDocumentProcessor(indentService) },
+                { XmlNodeType.CDATA, new CDATADocumentProcessor(indentService) },
+                // { XmlNodeType.EntityReference, null },
+                // { XmlNodeType.Entity, null },
+                { XmlNodeType.ProcessingInstruction, new ProcessInstructionDocumentProcessor(indentService) },
+                { XmlNodeType.Comment, new CommentDocumentProcessor(options, indentService) },
+                // { XmlNodeType.Document, null },
+                // { XmlNodeType.DocumentType, null },
+                // { XmlNodeType.DocumentFragment, null },
+                // { XmlNodeType.Notation, null },
+                { XmlNodeType.Whitespace, new WhitespaceDocumentProcessor() },
+                { XmlNodeType.SignificantWhitespace, new SignificantWhitespaceDocumentProcessor() },
+                { XmlNodeType.EndElement, new EndElementDocumentProcessor(options,indentService) },
+                // { XmlNodeType.EndEntity, null },
+                // ignoring xml declarations for Xamarin support
+                { XmlNodeType.XmlDeclaration, new XmlDeclarationDocumentProcessor() }
+            };
+        }
+
+        /// <summary>
+        /// Reads XML file's first node and processes its attributes in order to find
+        /// ignored namespaces.
+        /// </summary>
+        /// <param name="xamlSource">XAML file content</param>
+        /// <returns></returns>
+        private IList<string> FindIgnoredNamespaces(string xamlSource)
+        {
+            using (var sourceReader = new StringReader(xamlSource))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(sourceReader))
+                {
+                    // Try read first element
+                    while (!xmlReader.Read() || xmlReader.NodeType != XmlNodeType.Element) { }
+                    // Did not find any elements.
+                    if (xmlReader.EOF)
+                    {
+                        return Array.Empty<string>();
+                    }
+                    // Try to move to first attribute.
+                    if (!xmlReader.MoveToFirstAttribute())
+                    {
+                        // First element do not have any attributes.
+                        return Array.Empty<string>();
+                    }
+
+                    IList<string> ignoredNamespacesPrefixes = new List<string>();
+                    while (xmlReader.MoveToNextAttribute())
+                    {
+                        // Full namespace URI, it's stored in Value property.
+                        var prefix = xmlReader.LocalName;
+                        var namespaceUri = xmlReader.Value.Replace($"[{prefix}]", "");
+
+                        if (ignoredNamespacesInOrdering.Contains(namespaceUri))
+                        {
+                            ignoredNamespacesPrefixes.Add(prefix);
+                        }
+                    }
+
+                    return ignoredNamespacesPrefixes;
+                }
+            }
         }
 
         private string Format(string xamlSource)
